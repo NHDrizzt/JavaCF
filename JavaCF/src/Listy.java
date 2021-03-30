3172d3be14d005a9cea204edebcb7d4d22d870c8
Rokol <biel_s.carvalho2@hotmail.com>
2021-03-30 20:36:42 -0300

    HR List

--- JavaCF/src/Listy.java
+++ JavaCF/src/Listy.java
@@ -1,9 +1,31 @@
+import java.io.*;
+import java.util.*;
 
 public class Listy {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+        int  n = sc.nextInt();
+        LinkedList<Integer> list = new LinkedList<>();
+        for(int i=0; i<n;i++){
+            list.add(sc.nextInt());
+        }
+        System.out.println("ab");
+        int q = sc.nextInt();
+        for(int i=0; i<q; i++){
+            if(sc.next().equals("Insert")){
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                list.add(a,b);
+            }
+            else{
+                int c = sc.nextInt();
+                list.remove(c);
+            }
+        }
+        for (Integer num : list) {
+            System.out.print(num + " ");
+        }
 	}
 
 }
