b0d3d73fd0f900c6dc2fbc1ae984dd7b4f3ca588
Rokol <biel_s.carvalho2@hotmail.com>
2021-04-03 15:03:19 -0300

    ExcpHandle

--- JavaCF/src/ExceptionHand.java
+++ JavaCF/src/ExceptionHand.java
@@ -1,4 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class ExceptionHand {
 
+    public static void main(String[] args) {
+       try (Scanner scanner = new Scanner(System.in)) {
+          System.out.println(scanner.nextInt()/scanner.nextInt());
+        } catch (ArithmeticException ex) {
+            System.out.println(ex);
+        } catch (InputMismatchException e){
+            System.out.println(e.getClass().getName());   
+        }
+    }
 }
