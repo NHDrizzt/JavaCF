19a8dc6338ec3347da5858514a83c5e3d12e7d39
Rokol <biel_s.carvalho2@hotmail.com>
2021-04-03 15:03:08 -0300

    HandleExcp

--- JavaCF/src/JavaOOP/JavaIterator.java
+++ JavaCF/src/JavaOOP/JavaIterator.java
@@ -37,3 +37,15 @@ public class JavaIterator{
    }
 }
 
+/*
+ * ArrayList mylist = new ArrayList();
+        mylist.add("Hello");
+        mylist.add("Java");
+        mylist.add("4");
+        Iterator it = mylist.iterator();
+        while(it.hasNext()){
+            Object element = it.next();
+            System.out.println((String)element);
+        }
+ */
+
