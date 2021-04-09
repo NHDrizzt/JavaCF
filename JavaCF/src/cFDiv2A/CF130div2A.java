6a6d77e08d2dbeedc2a86defb2417dcb9b9e1d42
Rokol <biel_s.carvalho2@hotmail.com>
2021-04-08 21:52:21 -0300

    splittrim

---
+++ JavaCF/src/cFDiv2A/CF130div2A.java
@@ -0,0 +1,15 @@
+package cFDiv2A;
+
+import java.util.Scanner;
+
+public class CF130div2A {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		a= a.replace("WUB", " ");
+		System.out.println(a.trim());
+		
+	}
+
+}
