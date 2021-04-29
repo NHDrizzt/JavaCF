de4f131ff803e50128ad04e95bdd287b7a8bd3c5
Rokol <biel_s.carvalho2@hotmail.com>
2021-04-28 22:41:54 -0300

    cf172

---
+++ JavaCF/src/cFDiv2A/CF172div2A.java
@@ -0,0 +1,12 @@
+package cFDiv2A;
+
+import java.util.Scanner;
+
+public class CF172div2A {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String letters = sc.next();
+		System.out.println(letters.substring(0,1).toUpperCase() + letters.substring(1));
+	}
+}
