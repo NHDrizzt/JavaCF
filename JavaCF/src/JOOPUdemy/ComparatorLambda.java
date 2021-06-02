188a8a0a6c2ac4fb631e5873f5fbae13a803532b
Rokol <biel_s.carvalho2@hotmail.com>
2021-06-02 15:33:30 -0300

    Funções que recebem funções como parametro

--- JavaCF/src/JOOPUdemy/ComparatorLambda.java
+++ JavaCF/src/JOOPUdemy/ComparatorLambda.java
@@ -3,6 +3,7 @@ package JOOPUdemy;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import JOOPUdemy.model.entities.Product;
@@ -16,7 +17,19 @@ public class ComparatorLambda {
 		list.add(new Product("Note", 1100.0));
 		list.add(new Product("Test", 5200.0));
 		
-		List<String> lstring = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
-		lstring.forEach(t -> System.out.println(t));
+		String sum = filteredSum(list, p -> p.getValue() < 600);
+		System.out.println(sum);
+	}
+	
+	public static String filteredSum(List<Product> p, Predicate<Product> criteria) {
+		String sum = null;
+		for(Product x : p) {
+			if(criteria.test(x)) {
+				sum = x.getName();
+			}
+		}
+		return sum;
 	}
 }
+
+
